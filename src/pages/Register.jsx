91c8bb3bd78369e1caa8c9a9a3883cbdfb9b5a91
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-10-20 21:15:33 +0530

    register ui integration completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,41 @@
 import React, { useState } from 'react'
 import { Footer, Navbar } from "../components";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 const Register = () => {
-    const [role, setRole] = useState("seller");
+    // const [role, setRole] = useState("seller");
+
+    const [formData, setFormData] = useState({
+        fullName: "",
+        password: "",
+        role: "seller",
+        email: "",
+    });
+    const [error, setError] = useState(null); 
+    const navigate = useNavigate(); 
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log(formData);            
+            const response = await axios.post("http://localhost:5000/api/auth/signup", formData);
+            console.log(response);            
+            navigate("/login"); 
+
+        } catch (err) {
+            setError(err.response?.data?.error || "Registration failed");
+        }
+    };
+
     return (
         <>
             <Navbar />
@@ -11,14 +44,18 @@ const Register = () => {
                 <hr />
                 <div class="row my-4 h-100">
                     <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div class="form my-3">
                                 <label for="Name">Full Name</label>
                                 <input
-                                    type="email"
+                                    type="text"
                                     class="form-control"
                                     id="Name"
+                                    name="fullName"
+                                    value={formData.fullName}
+                                    onChange={handleChange}
                                     placeholder="Enter Your Name"
+                                    required
                                 />
                             </div>
                             <div class="form my-3">
@@ -27,7 +64,11 @@ const Register = () => {
                                     type="email"
                                     class="form-control"
                                     id="Email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
                                     placeholder="name@example.com"
+                                    required
                                 />
                             </div>
                             <div className="form my-3">
@@ -35,11 +76,9 @@ const Register = () => {
                                 <select
                                     id="roleSelect"
                                     className="form-control"
-                                    value={role}
-                                    onChange={(e) => {
-                                        console.log(e.target.value);                                        
-                                        setRole(e.target.value);
-                                    }}
+                                    name="role"
+                                    value={formData.role}
+                                    onChange={handleChange}
                                 >
                                     <option value="buyer">Register as Buyer</option>
                                     <option value="seller">Register as Seller</option>
@@ -51,14 +90,18 @@ const Register = () => {
                                     type="password"
                                     class="form-control"
                                     id="Password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
                                     placeholder="Password"
+                                    required
                                 />
                             </div>
                             <div className="my-3">
                                 <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
                             </div>
                             <div className="text-center">
-                                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                                <button class="my-2 mx-auto btn btn-dark" type="submit">
                                     Register
                                 </button>
                             </div>
