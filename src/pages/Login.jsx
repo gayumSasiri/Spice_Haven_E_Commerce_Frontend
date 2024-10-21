db79cdb3deb3467f4c092f2c92715ba931ffc299
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-10-21 08:51:44 +0530

    login implentation done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Footer, Navbar } from "../components";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      setError("");
+      // console.log(email,password);
+      
+      const response = await axios.post("http://localhost:5000/api/auth/login", {
+        password,
+        email,
+      });
+      console.log(response);
+      
+
+      if (response.data) {
+        localStorage.setItem("token", response.data.token);
+        toast.success(`Hello ${response.data.fullName}!`);
+        navigate("/");
+      }
+    } catch (err) {
+      setError(err.response?.data?.error || "An error occurred. Please try again.");
+
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -11,7 +46,7 @@ const Login = () => {
         <hr />
         <div class="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div class="my-3">
                 <label for="display-4">Email address</label>
                 <input
@@ -19,6 +54,9 @@ const Login = () => {
                   class="form-control"
                   id="floatingInput"
                   placeholder="name@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
               <div class="my-3">
@@ -28,13 +66,25 @@ const Login = () => {
                   class="form-control"
                   id="floatingPassword"
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </div>
+              {error && (
+                <p className="text-danger">
+                  {error}{"."}
+                  <br />
+                  <Link to="/resetpassword" className="text-decoration-underline text-info">
+                    Forgot your password?
+                  </Link>
+                </p>
+              )}
               <div className="my-3">
                 <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
               </div>
               <div className="text-center">
-                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                <button class="my-2 mx-auto btn btn-dark" type="submit">
                   Login
                 </button>
               </div>
